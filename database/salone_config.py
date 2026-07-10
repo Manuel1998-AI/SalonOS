7c808793c34a658557fd65233ed5f73bcec085f3
Manuel1998-AI <compamanu1998@gmail.com>
2026-07-11 00:37:26 +0200

    Create salone_config.py

---
+++ database/salone_config.py
@@ -0,0 +1,32 @@
+# Configurazione singolo salone SalonOS
+
+salone = {
+    "nome": "Nome Salone",
+
+    "orari": {
+        "lunedi": "09:00-18:30",
+        "martedi": "09:00-18:30",
+        "mercoledi": "09:00-18:30",
+        "giovedi": "09:00-18:30",
+        "venerdi": "09:00-18:30",
+        "sabato": "09:00-17:00"
+    },
+
+    "servizi": [
+        {
+            "nome": "Taglio uomo",
+            "prezzo": 25,
+            "durata": 30
+        },
+        {
+            "nome": "Taglio donna",
+            "prezzo": 40,
+            "durata": 60
+        },
+        {
+            "nome": "Colore",
+            "prezzo": 50,
+            "durata": 90
+        }
+    ]
+}
