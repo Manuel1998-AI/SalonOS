c65a1a357b761025bebeb4724ac2f0998f386258
Manuel1998-AI <compamanu1998@gmail.com>
2026-07-11 00:20:38 +0200

    Update main.py

--- backend/backend/app/main.py
+++ backend/backend/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from routes import prenotazioni
 
 app = FastAPI(
     title="SalonOS API",
     description="Backend principale della piattaforma SalonOS",
     version="0.1.0"
 )
+
+app.include_router(prenotazioni.router)
 
 
 @app.get("/")
