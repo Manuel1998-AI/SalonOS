36a56af7c09c942fc54feb2c6c7435d462dfbb3c
Manuel1998-AI <compamanu1998@gmail.com>
2026-07-11 19:18:30 +0200

    Update start.py

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     uvicorn.run(
-        "backend.app.main:app",
+        "backend.backend.app.main:app",
         host="0.0.0.0",
         port=8000
     )
